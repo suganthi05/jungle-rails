1c97476cc24615e40f52643f3feb7d253890dc1a
Suganthi Jayaseelan <suganthi1483@gmail.com>
2018-10-13 16:35:13 +0000

    Mail feature  at console

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+  def order_email(user_name, user_email, order, total_amount)
+      @user_name = user_name
+      @order = order
+      @total_amount = total_amount
+      mail(to: user_email, subject: "Thank you for your order ##{@order.id}")
+  end
+end
